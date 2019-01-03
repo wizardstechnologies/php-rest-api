b82dc4c1f1ea5dc6946f463abe9ea28704dfce2b
Romain BigZ Richard <rrichard@outlook.fr>
2019-01-03 10:27:41 +0100

    fix: pagination for arrays was throwing an error. add some tests

---
+++ tests/WizardsTest/Paginator/ArrayPagerfantaPaginatorTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WizardsTest\Paginator;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\Router;
+use WizardsRest\Paginator\ArrayPagerfantaPaginator;
+use WizardsRest\Parser\RestQueryParser;
+use Zend\Diactoros\ServerRequest;
+
+class ArrayPagerfantaPaginatorTest extends TestCase
+{
+    public function testPaginate()
+    {
+        $routerMock = $this->createMock(Router::class);
+        $paginator = new ArrayPagerfantaPaginator($routerMock);
+        $collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+        $request = new ServerRequest(
+            [],
+            [],
+            '/posts',
+            'GET'
+        );
+        $this->assertEquals(RestQueryParser::DEFAULT_LIMIT, count($paginator->paginate($collection, $request)));
+
+        $request2 = new ServerRequest(
+            [],
+            [],
+            '/posts',
+            'GET',
+            'php://input',
+            [],
+            [],
+            [RestQueryParser::PARAMETER_LIMIT => '5']
+        );
+        $this->assertEquals(5, count($paginator->paginate($collection, $request2)));
+    }
+}
